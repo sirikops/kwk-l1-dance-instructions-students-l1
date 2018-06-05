8f03c18161f6ab027673865b17523a2d1e8fac8f
sirikops <sirikops@hotmail.com>
2018-06-05 16:07:26 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,49 @@
 
 # Code your methods below
+def stance
+  puts "Plants legs far apart, bend knees slightly and keep posture loose."
+end
+stance
+
+def basefootwork
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip-step backward"
+end
+basefootwork
+
+def skipstep_left
+  puts "Lower left foot to the ground."
+  puts "Bounce left foot back up slightly, kicking it a few inches back."
+  puts "Left, Right Left and Left"
+end
+skipstep_left
+
+def skipstep_right
+  puts "Lower right foot to the ground."
+  puts "Bounce right foot back up slightly, kicking it a few inches back."
+  puts "Right, Left, Right and Right"
+end
+skipstep_right
+
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Life your arms up and down in a loose bouncing movement, in time with the beat of the song."
+  puts "This movement is repeated eight times."
+end
+bob_the_reins
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+  puts "This movement is repeated eight times as well."
+end
+lasso
+
+def bust_a_move
+  puts "Don't be afraid to go crazy!"
+end
+bust_a_move
+  
